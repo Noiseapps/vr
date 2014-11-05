81e8ceb91bfdfe6a16ce04cd02f49428e07a356f
Noiseapps <noiseapps@gmail.com>
2014-11-05 18:30:15 +0100

    wykrywanie kolizji
    otwieranie drzwi przy wykryciu kolizji z cubem
    2 cuby, 2 pary drzwi
    dodany pokój początkowy, korytarz do głównego i korytarz prowadzący do
    ostatniego pomieszczenia

--- unity_project/Assets/Flickering.js
+++ unity_project/Assets/Flickering.js
@@ -1,22 +1,25 @@
 ﻿#pragma strict
 
-var minTime : float = 1.0;
-var maxTime : float = 2.0;
+var minTime : float = 0.1;
+var maxTime : float = 1;
+var busy : int = 0;
 
 function Start () {
 }
 
 function Update () {
-	Flicker();
+	if(busy == 0){
+		Flicker();
+	}
 }
 
 function Flicker(){
+	busy = 1;
 	light.enabled = true;
 	var rand : float = Random.Range(minTime, maxTime);
-	Debug.Log(rand);
 	yield WaitForSeconds(rand);
 	light.enabled = false;
 	rand = Random.Range(minTime, maxTime);
-	Debug.Log(rand);
 	yield WaitForSeconds(rand);
+	busy = 0;
 }
