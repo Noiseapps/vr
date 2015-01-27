dd964dda7d881394f18e13555ee0b47e6c4bba6c
Tomasz Ścibiorek <noiseapps@gmail.com>
2015-01-27 14:52:59 +0100

    help texty

--- unity_project/Assets/Script/BackgroundSound.js
+++ unity_project/Assets/Script/BackgroundSound.js
@@ -11,17 +11,19 @@ function Awake () {
 }
 
 function Start() {
-	source.volume = .3f;
+	source.volume = .7f;
 	source.PlayOneShot(clip, .2f);
 }
 
 function playFireBall(){
+source.volume = .5f;
 source.Stop();
 source.clip = fireBall;
 source.Play(0);
 }
 
 function playEndingSong(){
+	source.volume = .7f;
 	source.Stop();
 	source.clip = ending;
 	source.Play(0);
